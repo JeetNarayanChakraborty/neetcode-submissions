1b95a8d53187fd7e22cda6d9c816af72234014f3
Jeet Narayan Chakraborty <68782923+JeetNarayanChakraborty@users.noreply.github.com>
2026-09-20 00:49:00 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,49 @@
+class MinStack 
+{
+    private int[] m;
+    private int[] aux;
+    private int minElement=Integer.MAX_VALUE;
+    private int top=-1;
+
+    public MinStack() 
+    {
+        m = new int[20];
+        aux = new int[20];
+        top = 0;
+
+        for(int i=0; i<m.length; i++) m[i] = Integer.MIN_VALUE;
+    }
+    
+    public void push(int val) 
+    {
+        if(top <= m.length-1)
+        {
+            m[top] = val;
+            minElement = Math.min(minElement, val);
+            aux[top] = minElement;
+            top++;
+        }
+    }
+    
+    public void pop() 
+    {
+        m[top] = 0;
+        aux[top] = 0;
+        top--;
+        minElement = aux[top];
+    }
+    
+    public int top() 
+    {
+        return m[top];
+    }
+    
+    public int getMin() 
+    {
+        return aux[top];
+    }
+}
+
+
+
+
