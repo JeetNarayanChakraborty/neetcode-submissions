dad305aab601c4e3215de264a3f57f6601782b2c
Jeet Narayan Chakraborty <68782923+JeetNarayanChakraborty@users.noreply.github.com>
2026-09-24 02:20:08 +0530

    Add: design-twitter-feed - submission-6

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-6.java
@@ -0,0 +1,85 @@
+public class userDetails
+{
+    Long timeStamp;
+    int userID;
+    int tweetID;
+
+    public userDetails(Long timeStamp, int userID, int tweetID)
+    {
+        this.timeStamp = timeStamp;
+        this.userID = userID;
+        this.tweetID = tweetID;
+    }
+}
+
+class Twitter 
+{
+    Map<Integer, HashSet<Integer>> userTweets;
+    Map<Integer, HashSet<Integer>> userFollowing;
+    PriorityQueue<userDetails> allPosts;
+    private Long time=0L;
+
+    public Twitter() 
+    {
+        userTweets = new HashMap<>();
+        userFollowing = new HashMap<>();
+        allPosts = new PriorityQueue<>
+                      ((a, b) -> Long.compare(b.timeStamp, a.timeStamp));
+    }
+    
+    public void postTweet(int userId, int tweetId) 
+    {
+        userTweets.computeIfAbsent(userId, k -> new HashSet<>()).add(tweetId);
+        allPosts.add(new userDetails(time++, userId, tweetId));
+    }
+    
+    public List<Integer> getNewsFeed(int userId) 
+    {
+        int l = allPosts.size();
+        ArrayList<userDetails> details = new ArrayList<>();
+        ArrayList<Integer> recentTweets = new ArrayList<>();
+        
+        for(int i=0; i<l; i++)
+        {
+            userDetails ud = allPosts.poll();
+            details.add(ud);
+
+            if(ud.userID == userId || userFollowing.containsKey(userId) && 
+               userFollowing.get(userId).contains(ud.userID)
+               && recentTweets.size() < 10)
+            {
+                recentTweets.add(ud.tweetID);
+            }
+        }
+
+        for(userDetails d : details) allPosts.add(d);
+
+        return recentTweets;
+    }
+    
+    public void follow(int followerId, int followeeId) 
+    {
+        userFollowing.computeIfAbsent(followerId, k -> new HashSet<>()).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) 
+    {
+        if(userFollowing.containsKey(followerId))
+        {
+            if(userFollowing.get(followerId).contains(followeeId)) 
+            {
+                userFollowing.get(followerId).remove(followeeId);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
